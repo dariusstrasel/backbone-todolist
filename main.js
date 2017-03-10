1b20c5a00aa8332caf049d4703c533102d42a38c
Darius Strasel <strasel.darius@gmail.com>
2017-03-10 18:10:48 -0500

    Added task name validation function.

--- main.js
+++ main.js
@@ -7,10 +7,42 @@ var createTaskButton_element = document.getElementById("createTask");
 var createTaskButton_onclick = createTaskButton_element.addEventListener("click", createTask);
 
 function createTask () {
-    console.log("createTask() invoked.");
-    var taskName_element = document.getElementById("taskName");
-    var taskName_value = taskName_element.value;
+    if (validFieldValue()) {
+        console.log("createTask() invoked.");
+        var taskName_element = document.getElementById("taskName");
+        var taskName_value = taskName_element.value;
 
-    // Grab the parent to append the new task into.
-    var taskList_element = document.getElementById("tasks");
+        // Create new task to append from form value.
+        var newTask = document.createElement("li");
+        var taskContent = document.createTextNode(taskName_value);
+        newTask.appendChild(taskContent);
+
+
+        // Grab the parent container to append the new task into.
+        var taskList_element = document.getElementById("tasks");
+
+        // Append task to parent
+        taskList_element.appendChild(newTask);
+    }
 }
+
+function validFieldValue() {
+    console.log("validFieldValue() invoked");
+    var createTaskButton_element = document.getElementById("taskName");
+    console.log(createTaskButton_element.value);
+    console.log(createTaskButton_element.value === "");
+    if (createTaskButton_element.value === "") {
+        console.log("No");
+        return false;
+    }
+    else {
+        console.log("Yes");
+        return true;
+    }
+}
+
+function generateTask() {
+
+}
+
+// taskName | deleteButton
